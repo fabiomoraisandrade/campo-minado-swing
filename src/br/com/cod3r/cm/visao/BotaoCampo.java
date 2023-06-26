f06e651268bc048dc1921d3312afdf675fb8b6fc
Fábio Morais de Andrade <fabio.adr12@gmail.com>
2023-06-26 20:24:19 -0300

    Interface Gráfica do Campo Minado #05

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -25,7 +25,7 @@ public class BotaoCampo extends JButton implements CampoObservador, MouseListene
 		this.campo = campo;
 		setBackground(BG_PADRAO);
 		setBorder(BorderFactory.createBevelBorder(0));
-		
+		setOpaque(true);
 		addMouseListener(this);
 		campo.registrarObservador(this);
 	}
@@ -48,23 +48,32 @@ public class BotaoCampo extends JButton implements CampoObservador, MouseListene
 	}
 
 	private void aplicarEstiloPadrao() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_PADRAO);
+		setText("");
 	}
 
 	private void aplicarEstiloExplodir() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_EXPLODIR);
+		setForeground(Color.WHITE);
+		setText("X");
 	}
 
 	private void aplicarEstiloMarcar() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_MARCAR);
+		setForeground(Color.BLACK);
+		setText("M");		
 	}
 
 	private void aplicarEstiloAbrir() {
-		setBackground(BG_PADRAO);
+		
 		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		
+		if(campo.isMinado()) {
+			setBackground(BG_EXPLODIR);
+			return;
+		}
+		
+		setBackground(BG_PADRAO);
 		
 		switch (campo.minasNaVizinhanca()) {
 		case 1:
